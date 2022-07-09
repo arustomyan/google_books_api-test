0853dfc0a71fc6c83e5c5e88038f14827d17621a
varustomyan <v.arustomyan@icloud.com>
2022-07-09 17:04:20 +0300

    feature: add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Header } from "./components/Header";
 import { BookDetails } from "./components/BookDetails/";
+import { Route, Routes } from "react-router-dom";
+import { BooksList } from "./components/BookList";
 
 function App() {
   return (
     <>
       <Header />
-      <BookDetails />
+      <Routes>
+        <Route path="/" element={<BooksList />} />
+        <Route path="/detail/:id" element={<BookDetails />} />
+      </Routes>
     </>
   );
 }
