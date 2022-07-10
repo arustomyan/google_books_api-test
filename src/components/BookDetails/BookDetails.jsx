0b05bfc80f018b57f517122b4bfc0174d5bc80c6
varustomyan <v.arustomyan@icloud.com>
2022-07-10 03:19:20 +0300

    feature: add loader in BookDetails.jsx

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from "react";
-
 import style from "./BookDetails.module.css";
-import books from "../../fakeData.json";
 import { useParams } from "react-router-dom";
 import RestApi from "../../api/api";
+import Loader from "../Loader/Loader";
 
 function BookDetails() {
-  const bookq = books.items[20];
   const params = useParams();
-  // eslint-disable-next-line no-unused-vars
-  const [book, setBooks] = useState(bookq);
+  const [book, setBooks] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchBook = async () => {
     return await RestApi.getBook(params.id);
@@ -18,32 +16,37 @@ function BookDetails() {
   useEffect(() => {
     fetchBook().then((res) => {
       setBooks(res);
-      console.log(res);
+      setIsLoading(false);
     });
   }, [params]);
 
-  console.log(params);
   return (
     <div className={style.component}>
-      <div className={style.cover}>
-        <div className={style.imageWrapper}>
-          <img
-            className={style.image}
-            src={book.volumeInfo.imageLinks.thumbnail}
-            alt=""
-          />
-        </div>
-      </div>
-      <div className={style.details}>
-        <a className={style.category}>
-          {book.volumeInfo.categories ? book.volumeInfo.categories[0] : ""}
-        </a>
-        <p className={style.title}>{book.volumeInfo.title}</p>
-        <p className={style.authors}>
-          {book.volumeInfo.authors ? book.volumeInfo.authors[0] : ""}
-        </p>
-        <p>{book.volumeInfo.description}</p>
-      </div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <div className={style.cover}>
+            <div className={style.imageWrapper}>
+              <img
+                className={style.image}
+                src={book.volumeInfo.imageLinks.thumbnail}
+                alt={`Cover of ${book.volumeInfo.title}`}
+              />
+            </div>
+          </div>
+          <div className={style.details}>
+            <a className={style.category}>
+              {book.volumeInfo.categories ? book.volumeInfo.categories[0] : ""}
+            </a>
+            <p className={style.title}>{book.volumeInfo.title}</p>
+            <p className={style.authors}>
+              {book.volumeInfo.authors ? book.volumeInfo.authors[0] : ""}
+            </p>
+            <p>{book.volumeInfo.description}</p>
+          </div>
+        </>
+      )}
     </div>
   );
 }
